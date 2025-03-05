4a127850b79143c24c272e83aaa883be2f31fe09
kevinwu0213 <wukevin365@gmail.com>
2025-03-05 12:39:23 +0800

    week03

---
+++ week03-3  (1).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 電腦的 for 迴圈，從 0 開始
+    for (int i = 0; i < 5; i++) {
+        printf("%d", i);
+    }
+
+    printf("\n"); // 換行
+
+    // 人的 for 迴圈，從 1 開始
+    for (int i = 1; i <= 5; i++) {
+        printf("%d", i);
+    }
+
+    return 0;
+}
